4701f966688d6c58a956ee7dd1cd60efff9ea0c1
borozabalad <zabalad@borogroup.co.uk>
2025-02-23 18:55:39 +1000

    added _redirects file

--- ai_portfolio_labs_react_frontend_base/b_source/src/App.js
+++ ai_portfolio_labs_react_frontend_base/b_source/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./templates/home_page.js";
 import AiModelsInferenceTimesResults from "./templates/ai_models_inference_times_results.js";
-import "./App.css";
+import NotFound from "./templates/not_found.js";
 
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/ai_battle" element={<AiModelsInferenceTimesResults />} />
+        <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
       </Routes>
     </Router>
   );
